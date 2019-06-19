fe79017732dde0fbcb235cbd1567c739005db379
passin <zengbinbinpassin@foxmail.com>
2019-06-19 22:49:25 +0800

    feat: v1.0.1--对 LoadLayout 添加 detch 安全判断。

--- loadknife/src/main/java/me/passin/loadknife/core/LoadLayout.java
+++ loadknife/src/main/java/me/passin/loadknife/core/LoadLayout.java
@@ -14,42 +14,39 @@ import me.passin.loadknife.utils.LoadKnifeUtils;
 /**
  * @author: zbb 33775
  * @date: 2019/3/19 21:59
- * @hide
  */
 public class LoadLayout extends FrameLayout {
 
     private static final Map<Class<? extends Callback>, Callback> callbacks = new HashMap<>();
 
     private Map<Class<? extends Callback>, ServiceView> mServiceViewMap = new ConcurrentHashMap<>();
-    private Context context;
-    private Callback.OnReloadListener onReloadListener;
-    private Class<? extends Callback> curCallbackClass;
+    private Context mContext;
+    private Callback.OnReloadListener mOnReloadListener;
+    private Class<? extends Callback> mCurCallbackClass;
+    private boolean mIsDetach;
 
-    public LoadLayout(@NonNull Context context, Callback.OnReloadListener onReloadListener) {
-        this(context);
-        this.context = context;
-        this.onReloadListener = onReloadListener;
-    }
-
-    public LoadLayout(@NonNull Context context) {
+    LoadLayout(@NonNull Context context) {
         super(context);
     }
 
-    /**
-     * @hide
-     */
-    public static void addCallback(Callback... callbacks) {
+    public LoadLayout(@NonNull Context context, Callback.OnReloadListener onReloadListener) {
+        this(context);
+        this.mContext = context;
+        this.mOnReloadListener = onReloadListener;
+    }
+
+    static void addCallback(Callback... callbacks) {
         for (Callback callback : callbacks) {
             LoadLayout.callbacks.put(callback.getClass(), callback);
         }
     }
 
-    public void initSuccessView(View view) {
+    void initSuccessView(View view) {
         mServiceViewMap.put(SuccessCallback.class, new ServiceView(view));
         addView(view);
     }
 
-    public void showCallback(final Class<? extends Callback> callback) {
+    void showCallback(final Class<? extends Callback> callback) {
         if (LoadKnifeUtils.isMainThread()) {
             showCallbackView(callback);
         } else {
@@ -63,12 +60,15 @@ public class LoadLayout extends FrameLayout {
     }
 
     private void showCallbackView(Class<? extends Callback> preCallbackClass) {
-        if (curCallbackClass == preCallbackClass) {
+        if (mCurCallbackClass == preCallbackClass) {
+            return;
+        }
+        if (mIsDetach) {
             return;
         }
         if (getChildCount() > 1) {
-            Callback curCallback = getCallback(curCallbackClass);
-            curCallback.onDetach(context, getServiceView(curCallbackClass).getViewHelper());
+            Callback curCallback = getCallback(mCurCallbackClass);
+            curCallback.onDetach(mContext, getServiceView(mCurCallbackClass).getViewHelper());
             removeViewAt(1);
         }
         ServiceView preServiceView = getServiceView(preCallbackClass);
@@ -78,12 +78,11 @@ public class LoadLayout extends FrameLayout {
             Callback preCallback = getCallback(preCallbackClass);
             addView(preServiceView.getRootView());
             getServiceView(SuccessCallback.class).setVisibility(preCallback.successViewVisible());
-            preCallback.onAttach(context, preServiceView.getViewHelper());
+            preCallback.onAttach(mContext, preServiceView.getViewHelper());
         }
 
-        curCallbackClass = preCallbackClass;
+        mCurCallbackClass = preCallbackClass;
     }
-
 
     private Callback getCallback(Class<? extends Callback> callbackClass) {
         Callback callback = callbacks.get(callbackClass);
@@ -104,28 +103,38 @@ public class LoadLayout extends FrameLayout {
         return callback;
     }
 
-
     private ServiceView getServiceView(Class<? extends Callback> callbackClass) {
         ServiceView serviceView = mServiceViewMap.get(callbackClass);
         if (serviceView == null) {
             Callback callback = getCallback(callbackClass);
             // 构建 rootView
-            View rootView = View.inflate(context, callback.getLayoutId(), null);
+            View rootView = View.inflate(mContext, callback.getLayoutId(), null);
             ViewHelper viewHelper = new ViewHelper(rootView);
 
-            serviceView = new ServiceView(rootView, viewHelper, onReloadListener);
-            callback.onViewCreate(context, viewHelper);
+            serviceView = new ServiceView(rootView, viewHelper, mOnReloadListener);
+            callback.onViewCreate(mContext, viewHelper);
             mServiceViewMap.put(callbackClass, serviceView);
         }
         return serviceView;
     }
 
-    public Class<? extends Callback> getCurrentCallback() {
-        return curCallbackClass;
+    Class<? extends Callback> getCurrentCallback() {
+        return mCurCallbackClass;
     }
 
-    public ViewHelper getViewHelper(Class<? extends Callback> callback) {
+    ViewHelper getViewHelper(Class<? extends Callback> callback) {
         return getServiceView(callback).getViewHelper();
     }
 
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mIsDetach = false;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mIsDetach = true;
+    }
 }
